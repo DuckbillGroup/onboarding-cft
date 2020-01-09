12e083a1222af6c42fea79309af13876b0db92c0
Jesse DeRose <jessederose@JDR-DBG-MacBookPro13.local>
2020-01-09 12:27:17 -0800

    Initial commit.

---
+++ terraform/duckbill-cft-iam-role.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_iam_policy_document" "DuckbillGroup_CFT_AssumeRole_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["607466170983"]
+    }
+  }
+}
+
+resource "aws_iam_role" "DuckbillGroup_CFT_role" {
+  name               = "DuckbillGroup-CFT"
+  assume_role_policy = data.aws_iam_policy_document.DuckbillGroup_CFT_AssumeRole_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "DuckbillGroup_CFT_role_attachment" {
+  role       = aws_iam_role.DuckbillGroup_CFT_role.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
